74893d5751f9333260ef468e85e4c554d48794eb
gbevuakoe1 <emmaaddaedoe@gmail.com>
2025-09-15 17:37:45 -0700

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,10 @@
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+const links = document.querySelector(".nav-wrapper");
+
+openBtn.addEventListener("click", () =>{
+    links.classList.add("menu-open");
+});
+closeBtn.addEventListener("click", () =>{
+    links.classList.remove("menu-open")
+});
